11bc9ec577f0dbf112cdbedc494fffe7a9889f3a
Hao Xiang Liew <haoxiangliew@gmail.com>
2022-05-21 19:39:09 -0400

    fix git

--- git-wsl.sh
+++ git-wsl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager-core.exe"
 echo 'pinentry-program "/mnt/c/Program Files (x86)/GnuPG/bin/pinentry-basic.exe"' >> ~/.gnupg/gpg-agent.conf
